6c35b7b271e3be65b07843b631972a1ce300cea0
SausageMania <joshua5451@naver.com>
2021-05-20 17:35:15 +0900

    무한 렌더링 수정 및 배열화(useMemo 사용)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Paper, Grid, Button } from '@material-ui/core';
+import React, { useState, useEffect, useMemo } from 'react';
+import { Box, Paper, Grid, Button, Typography } from '@material-ui/core';
 import { Progress } from 'antd';
 import BezierEasing from 'bezier-easing';
 import CardFlip from 'react-card-flip';
@@ -7,74 +7,117 @@ import 'antd/dist/antd.css';
 
 const App = () => {
     const easing = BezierEasing(0.76, 0, 0.24, 1);
-    const maxNumber = 80;
+    const maxNumber = useMemo(() => [80, 40], []);
 
-    const [number, setNumber] = useState(0);
+    const [number, setNumber] = useState([0, 0]);
     const [buttonClick, setButtonClick] = useState(false);
 
     useEffect(() => {
         const increase = () => {
-            if (number < maxNumber) return setTimeout(() => setNumber(number + 1), 10);
+            return setTimeout(() => {
+                const copyNumber = number.slice();
+                copyNumber.forEach((num, index) => {
+                    if (num < maxNumber[index])
+                        copyNumber.splice(index, 1, num + maxNumber[index] / 100);
+                });
+                setNumber(copyNumber);
+            }, 10);
         };
         const decrease = () => {
-            if (number > 0) return setTimeout(() => setNumber(number - 1), 10);
+            return setTimeout(() => {
+                const copyNumber = number.slice();
+                copyNumber.forEach((num, index) => {
+                    if (num > 0) copyNumber.splice(index, 1, num - maxNumber[index] / 100);
+                });
+                setNumber(copyNumber);
+            }, 10);
         };
 
-        if (buttonClick) increase();
-        else decrease();
+        if (buttonClick && maxNumber[1] > number[1]) increase();
+        else if (!buttonClick && number[1] !== 0) decrease();
 
         return () => {
             if (buttonClick) clearTimeout(increase);
             else clearTimeout(decrease);
         };
-    }, [number, buttonClick]);
+    }, [number, maxNumber, buttonClick]);
 
     return (
         <Grid item xs={8}>
-            <CardFlip isFlipped={!buttonClick} flipDirection="horizontal">
-                <Box
-                    width="100%"
-                    height="500px"
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    flexDirection="column"
-                    component={Paper}
-                    elevation={12}
-                >
-                    <Box>프로젝트 관여</Box>
-                    <Progress
-                        type="circle"
-                        percent={maxNumber * easing(number / maxNumber)}
-                        status="active"
-                        strokeColor={{ '0%': '#ffe200', '100%': '#ff0000' }}
-                        format={() => `${number}%`}
-                        width={80}
-                    />
+            <Box display="flex" justifyContent="center">
+                <CardFlip isFlipped={!buttonClick} flipDirection="horizontal">
+                    <Box
+                        width="700px"
+                        height="500px"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        flexDirection="column"
+                        component={Paper}
+                        elevation={12}
+                    >
+                        <Box display="flex" height="150px">
+                            <Box m={3} display="flex" flexDirection="column" alignItems="center">
+                                <Box>프로젝트 진행률</Box>
+                                <Progress
+                                    type="circle"
+                                    percent={maxNumber[0] * easing(number[0] / maxNumber[0])}
+                                    status="active"
+                                    strokeColor={{ '0%': '#ffe200', '100%': '#ff0000' }}
+                                    format={() => `${parseInt(number[0])}%`}
+                                    width={80}
+                                    strokeWidth={10}
+                                />
+                            </Box>
+                            <Box m={3} display="flex" flexDirection="column" alignItems="center">
+                                <Box>프로젝트 관여율</Box>
+                                <Progress
+                                    type="circle"
+                                    percent={maxNumber[1] * easing(number[1] / maxNumber[1])}
+                                    status="active"
+                                    strokeColor={{ '0%': '#ffe200', '100%': '#ff0000' }}
+                                    format={() => `${parseInt(number[1])}%`}
+                                    width={80}
+                                    strokeWidth={10}
+                                />
+                            </Box>
+                        </Box>
 
-                    <Box m={2}>
-                        <Button variant="contained" onClick={() => setButtonClick(!buttonClick)}>
-                            카드 뒷면 열기
-                        </Button>
+                        <Box m={2}>
+                            <Button
+                                variant="contained"
+                                onClick={() => setButtonClick(!buttonClick)}
+                            >
+                                카드 뒷면 열기
+                            </Button>
+                        </Box>
                     </Box>
-                </Box>
-                <Box
-                    width="100%"
-                    height="500px"
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    flexDirection="column"
-                    component={Paper}
-                    elevation={12}
-                >
-                    <Box m={2}>
-                        <Button variant="contained" onClick={() => setButtonClick(!buttonClick)}>
-                            카드 앞면 열기
-                        </Button>
+                    <Box
+                        width="700px"
+                        height="500px"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        flexDirection="column"
+                        component={Paper}
+                        elevation={12}
+                    >
+                        <Box height="150px" display="flex" alignItems="center">
+                            <Typography style={{ fontSize: '24px', fontWeight: 600 }}>
+                                프로젝트 진행상황
+                            </Typography>
+                        </Box>
+                        <Box m={2}>
+                            <Button
+                                variant="contained"
+                                onClick={() => setButtonClick(!buttonClick)}
+                            >
+                                카드 앞면 열기
+                            </Button>
+                        </Box>
                     </Box>
-                </Box>
-            </CardFlip>
+                </CardFlip>
+            </Box>
         </Grid>
     );
 };
